cb04fe64ce8e6f3c9e00ba7b9b8b75446ee4dbaa
brian-victor254 <brianviqter31@gmail.com>
2024-03-22 22:43:29 +0300

    complete arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2
+console.log (multiply)
+
+let random = Math.floor(Math.random() * 10)+1
+
+let num3 = 9
+let num4 = 5
+let mod = num3 % num4
+console.log (mod)
+
+let max = Math.max(15,17,20)
